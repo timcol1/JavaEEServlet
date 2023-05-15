dc3f5fc039601b687a5167bb1dd5988d72a1953f
timcol1 <avlyakulovtimur@gmail.com>
2023-05-15 09:55:57 +0300

    add example with connect to db and print it on site

--- src/main/java/avlyalkulov/timur/javaeetest/servlets/TimeServlet.java
+++ src/main/java/avlyalkulov/timur/javaeetest/servlets/TimeServlet.java
@@ -1,11 +1,9 @@
 package avlyalkulov.timur.javaeetest.servlets;
 
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
---
+++ src/main/java/avlyalkulov/timur/javaeetest/servlets/db/BookServlet.java
@@ -0,0 +1,40 @@
+package avlyalkulov.timur.javaeetest.servlets.db;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+public class BookServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String url = "jdbc:postgresql://localhost:5432/jdbc-test";
+        String name = "postgres";
+        String pass = "1504";
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        try (Connection connection = DriverManager.getConnection(url, name, pass)) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("Select title from shop");
+            PrintWriter pw = response.getWriter();
+            while (resultSet.next())
+                pw.println("<p>" + resultSet.getString(1) + "</p>");
+            statement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
